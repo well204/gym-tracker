aa3fc7cab8e1ca807715b6ce58446fd53528f085
well204 <well.santos@alu.ufc.br>
2024-12-23 14:26:21 -0300

    first commit

---
+++ backend/gym-tracker/src/test/java/com/zeussd/gym_tracker/GymTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeussd.gym_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
