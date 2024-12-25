8661180f8e925b64b3be183c8967fe784e8bac7e
well204 <well.santos@alu.ufc.br>
2024-12-25 10:46:12 -0300

    adicionando a classe User

---
+++ backend/gym-tracker/src/main/java/com/zeussd/gym_tracker/entities/User.java
@@ -0,0 +1,89 @@
+package com.zeussd.gym_tracker.entities;
+
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long userId;
+    private String userName;
+    private String password;
+    private Double userWeight;
+    private Double userHeight;
+
+    public User() {}
+
+    public User(Long userId, String userName, String password, Double userWeight,
+                Double userHeight) {
+        super();
+        this.userId = userId;
+        this.userName = userName;
+        this.password = password;
+        this.userWeight = userWeight;
+        this.userHeight = userHeight;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Double getUserHeight() {
+        return userHeight;
+    }
+
+    public void setUserHeight(Double userHeight) {
+        this.userHeight = userHeight;
+    }
+
+    public Double getUserWeight() {
+        return userWeight;
+    }
+
+    public void setUserWeight(Double userWeight) {
+        this.userWeight = userWeight;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void shareProfile() {}
+
+    public void logout() {}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(userId, user.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((userId == null) ? 0 : userId.hashCode());
+        return result;
+    }
+}
---
+++ backend/gym-tracker/src/main/java/com/zeussd/gym_tracker/resources/UserResource.java
@@ -0,0 +1,12 @@
+package com.zeussd.gym_tracker.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value ="/users")
+public class UserResource {
+
+
+}
