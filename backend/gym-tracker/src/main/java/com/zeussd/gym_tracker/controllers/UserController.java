a54c30f1f1a557d5cec0b280fe36c28b3d027276
well204 <well.santos@alu.ufc.br>
2024-12-26 17:09:53 -0300

    ajustes

--- backend/gym-tracker/src/main/java/com/zeussd/gym_tracker/controllers/UserController.java
+++ backend/gym-tracker/src/main/java/com/zeussd/gym_tracker/controllers/UserController.java
@@ -1,12 +1,11 @@
-package com.zeussd.gym_tracker.resources;
+package com.zeussd.gym_tracker.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value ="/users")
-public class UserResource {
+public class UserController {
 
 
 }
---
+++ backend/gym-tracker/src/main/java/com/zeussd/gym_tracker/entities/Set.java
@@ -0,0 +1,10 @@
+package com.zeussd.gym_tracker.entities;
+import java.io.Serial;
+import java.io.Serializable;
+
+public class Set implements Serializable{
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private Integer reps;
+    private Integer prevReps;
+}
