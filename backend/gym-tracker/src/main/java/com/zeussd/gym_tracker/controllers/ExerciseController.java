f1965f19446f30abb0f537b58ed8027331b650e3
Darlan Ferreira <darlan.oliveiraferreira1@gmail.com>
2025-03-16 12:03:31 -0300

    endpoint delete, getById de exercise

--- backend/gym-tracker/src/main/java/com/zeussd/gym_tracker/controllers/ExerciseController.java
+++ backend/gym-tracker/src/main/java/com/zeussd/gym_tracker/controllers/ExerciseController.java
@@ -1,42 +1,72 @@
-package com.zeussd.gym_tracker.controllers;
+	package com.zeussd.gym_tracker.controllers;
 
-import java.util.List;
+	import java.util.List;
+	import java.util.Optional;
+	import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.http.ResponseEntity;
+	import org.springframework.web.bind.annotation.DeleteMapping;
+	import org.springframework.web.bind.annotation.GetMapping;
+	import org.springframework.web.bind.annotation.PathVariable;
+	import org.springframework.web.bind.annotation.PostMapping;
+	import org.springframework.web.bind.annotation.RequestBody;
+	import org.springframework.web.bind.annotation.RequestMapping;
+	import org.springframework.web.bind.annotation.RestController;
 
-import com.zeussd.gym_tracker.entities.Exercise;
-import com.zeussd.gym_tracker.repository.ExerciseRepository;
+	import com.zeussd.gym_tracker.entities.Exercise;
+	import com.zeussd.gym_tracker.repository.ExerciseRepository;
 
 
 
-@RestController
-@RequestMapping(value = "/exercises")
-public class ExerciseController {
-	
-	
-	// private static final Logger logger = LoggerFactory.getLogger(ExerciseController.class);
+	@RestController
+	@RequestMapping(value = "/exercises")
+	public class ExerciseController {
+		
+		
+		// private static final Logger logger = LoggerFactory.getLogger(ExerciseController.class);
 
-	@Autowired
-	private ExerciseRepository exerciseRepository;
+		@Autowired
+		private ExerciseRepository exerciseRepository;
 
-	@GetMapping
-	public ResponseEntity<List<Exercise>> getAllExercises() {
-		List<Exercise> exercises = exerciseRepository.findAll();
-		return ResponseEntity.ok(exercises);
+		@GetMapping
+		public ResponseEntity<List<Exercise>> getAllExercises() {
+			List<Exercise> exercises = exerciseRepository.findAll();
+			return ResponseEntity.ok(exercises);
+		}
+
+		@GetMapping("/{id}")
+		public ResponseEntity<Exercise> getExerciseById(@PathVariable(value="id") UUID id){
+			Optional<Exercise> exercise = exerciseRepository.findById(id);
+
+			if (exercise.isPresent()){
+				return ResponseEntity.ok(exercise.get());
+			}else{
+				return ResponseEntity.notFound().build();
+			}
+		}
+		
+		@PostMapping
+		public ResponseEntity<Exercise> createExercise (@RequestBody Exercise exercise){
+			// logger.info("Received exercise: " + exercise.toString());
+			Exercise savedExercise = exerciseRepository.save(exercise);
+			return ResponseEntity.ok(savedExercise);
+		}
+
+		@DeleteMapping("/{id}")
+		public ResponseEntity<Void> deleteExercise(@PathVariable(value = "id") UUID id){
+			Optional<Exercise> exercise = exerciseRepository.findById(id);
+
+			if (exercise.isPresent()){
+
+				exerciseRepository.delete(exercise.get());
+				return ResponseEntity.noContent().build();
+			
+			}else{
+				return ResponseEntity.notFound().build();
+			}
+
+		}
+		
+
 	}
-	
-	@PostMapping
-	public ResponseEntity<Exercise> createExercise (@RequestBody Exercise exercise){
-		// logger.info("Received exercise: " + exercise.toString());
-		Exercise savedExercise = exerciseRepository.save(exercise);
-		return ResponseEntity.ok(savedExercise);
-	}
-	
-
-}
