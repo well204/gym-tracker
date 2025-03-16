1f57d61dff0414d28061e513606611e4682272fa
well204 <well.santos@alu.ufc.br>
2025-03-16 07:35:05 -0300

    duration of workout

--- backend/gym-tracker/src/main/java/com/zeussd/gym_tracker/entities/Workout.java
+++ backend/gym-tracker/src/main/java/com/zeussd/gym_tracker/entities/Workout.java
@@ -1,29 +1,43 @@
 package com.zeussd.gym_tracker.entities;
 
-import java.io.Serializable;
-import java.util.List;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Table
-public class Workout implements Serializable {
+@Table(name="workout")
+public class Workout {
     @Id
     @GeneratedValue
     private UUID id;
-    private long workoutId;
     private String workoutName;
-    private long duration;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
     private boolean finished;
-    private List<Exercise> exercises;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    public long getDuration() {
+        if (startTime == null)
+            return 0;
+        LocalDateTime end = (endTime != null) ? endTime : LocalDateTime.now();
+        return  Duration.between(startTime, end).toMillis();
+    }
 }
